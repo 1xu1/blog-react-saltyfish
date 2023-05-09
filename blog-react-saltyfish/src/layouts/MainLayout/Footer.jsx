71a652a52e6617f727ceb2610e169a71ccae71e7
Xu Yuxiang <BG517748@800best.net>
2023-05-09 15:23:19 +0800

    增加footer

---
+++ blog-react-saltyfish/src/layouts/MainLayout/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a>&copy; 2023 盐巴鱼</a>
+      <i class="face-smile"></i>
+      <a>-版权所有.</a><br />
+      <a class="ebsite-Numbers" href="http://beian.miit.gov.cn/" target="_blank"
+        >浙ICP备2021004648号-1</a
+      >
+    </footer>
+  )
+}
