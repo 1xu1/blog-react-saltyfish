1daf734b80709a7342c9962860bf4e221354da64
Xu Yuxiang <BG517748@800best.net>
2023-08-02 17:19:47 +0800

    feat.增加博文分页查询

--- blog-react-saltyfish/src/app/api/blog/getBlogList/route.js
+++ blog-react-saltyfish/src/app/api/blog/getBlogList/route.js
@@ -3,9 +3,13 @@ import { m_blog } from '@/db/schema.js'
 import { NextResponse } from 'next/server';
 import { desc } from "drizzle-orm";
 
-export async function GET() {
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const limit = searchParams.get('limit') || 10
+  const offset = searchParams.get('offset') || 0
+
   try {
-    const data = await db.select().from(m_blog).orderBy(desc(m_blog.blogTime))
+    const data = await db.select().from(m_blog).orderBy(desc(m_blog.blogTime)).limit(limit).offset(offset)
     return NextResponse.json({ data })
   } catch (error) {
     return NextResponse.json({ error: error?.message ?? error}, { status: 500 })
