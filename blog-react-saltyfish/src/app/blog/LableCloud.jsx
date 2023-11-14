f0b1f129459b9d30c198fa13879a24a542fa3c7f
unknown <983970814@qq.com>
2023-11-14 21:49:30 +0800

    feat.更改样式

--- blog-react-saltyfish/src/app/blog/LableCloud.jsx
+++ blog-react-saltyfish/src/app/blog/LableCloud.jsx
@@ -10,11 +10,11 @@ export default function LableCloud(props) {
   return (
     <div className="w-full border bg-white p-4 rounded-md	">
       <div>
-        <p className=' text-2xl font-semibold'>标签云</p>
-        <div className='flex flex-row flex-wrap'>
+        <p className=' text-2xl font-semibold mb-2'>标签云</p>
+        <div className='flex flex-row flex-wrap gap-y-2'>
           {labels.map(label => {
             return (
-              <Link href={`/blog?label=${label.name}`} key={label.name} className=' me-2 p-1 hover:bg-slate-100 rounded-md transition duration-300 ease-in-out'>
+              <Link href={`/blog?label=${label.name}`} key={label.name} className=' me-2 p-1 hover:bg-slate-100 rounded-md transition duration-300 ease-in-out border'>
                 <span>{`${label?.name ?? ''}`}</span>
                 <span>{`${label?.num ?? ''}`}</span>
               </Link>
