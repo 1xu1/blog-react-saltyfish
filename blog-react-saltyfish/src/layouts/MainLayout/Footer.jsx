97c8d057b0d61f504149775dfebc2adf3fe9b949
saltyFish <983970814qq.com>
2023-11-15 14:37:21 +0800

    bugfix.修改一处链接抓取错误

--- blog-react-saltyfish/src/layouts/MainLayout/Footer.jsx
+++ blog-react-saltyfish/src/layouts/MainLayout/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="mx-auto flex items-center justify-center my-5 text-white">
-      <a>&copy; 2023 盐巴鱼</a>
+      <span>&copy; 2023 盐巴鱼</span>
       <i className="face-smile" />
-      <a>-版权所有.</a>
+      <span>-版权所有.</span>
       <br />
       <a className="ebsite-Numbers" href="http://beian.miit.gov.cn/" target="_blank">浙ICP备2021004648号-1</a>
     </footer>
