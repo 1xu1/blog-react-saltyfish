d5b222b8276b7d8bee37d7432a9d23351e4ee197
saltyFish <983970814qq.com>
2024-01-18 15:08:42 +0800

    remove beian

--- blog-react-saltyfish/src/layouts/MainLayout/Footer.jsx
+++ blog-react-saltyfish/src/layouts/MainLayout/Footer.jsx
@@ -4,8 +4,6 @@ export default function Footer() {
       <span>&copy; 2023 盐巴鱼</span>
       <i className="face-smile" />
       <span>-版权所有.</span>
-      <br />
-      <a className="ebsite-Numbers" href="http://beian.miit.gov.cn/" target="_blank">浙ICP备2021004648号-1</a>
     </footer>
     )
 }
