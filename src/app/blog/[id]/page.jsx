856ece0ebc2732eb49b8e047f2639057b117a4b9
saltyFish <983970814@qq.com>
2024-02-01 20:05:21 +0800

    feat.修复登陆头像404的问题

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -6,7 +6,6 @@ import BlogHeader from './BlogHeader'
 import WebsiteCounter from '@/components/WebsiteCounter/index'
 import LeftSide from './LeftSide'
 import CommentBlock from './CommentBlock'
-import Head from 'next/head'
 
 async function getData(params) {
   const blogId = params['id']
@@ -34,9 +33,6 @@ export default async function Page({ params }) {
 
   return (
     <MainLayout>
-      <Head>
-        <title key={'title'}>{blog.blogTitle ?? '盐巴鱼的博客'}</title>
-      </Head>
       <div className='flex min-h-screen flex-row justify-center mx-auto'>
         <aside className='max-lg:hidden w-1/4 max-w-xs relative'>
           <span className='sticky top-1/2 flex flex-row-reverse'>
